59d43158625e788470285dae2fd9a050552edd83
Brittany <bnbradshaw911@gmail.com>
2017-08-09 09:30:13 -0500

    updated parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+
   resources :products
-  get 'say/hello'
-
-  get 'say/goodbye'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see
+  #http://guides.rubyonrails.org/routing.html
 end
